b760b4e8cb53ce348d69ad2e5533e7bf7f33c429
alexandr matiugin <alexander.matiugin@pandadoc.com>
2018-09-05 22:50:08 +0300

    implementation selection selection sort algorithm

---
+++ selectionSort.js
@@ -0,0 +1,40 @@
+const arrSort = [
+  { name: 'wilco', views: 111 },
+  { name: 'Radio head', views: 156 },
+  { name: 'the black keys', views: 35 },
+  { name: 'Kishore kumar', views: 141 },
+  { name: 'the strokes', views: 61 },
+  { name: 'neutral milk hotel', views: 94 },
+  { name: 'beck', views: 88 },
+];
+
+
+const maxIdx = (arr) => {
+  let max = { views: 0 };
+
+  let idx = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].views > max.views) {
+      max = arr[i];
+      idx = i;
+    }
+  }
+  return idx;
+}
+
+const selectionSort = (arr) => {
+  const newArr = [];
+
+  for (let i = arr.length; i > 0; i--) {
+    let index = maxIdx(arr);
+    newArr.push(arr[index]);
+    arr.splice(index, 1);
+  }
+
+  return newArr;
+}
+
+console.log(
+  selectionSort(arrSort)
+);
