33f50e9cbdf662ad0210c7dc017386f485e89829
alexandr matiugin <alexander.matiugin@pandadoc.com>
2018-09-08 12:15:11 +0300

    fix code style

--- 04_quickSort/quickSort.js
+++ 04_quickSort/quickSort.js
@@ -1,28 +1,15 @@
 const quickSort = (arr) => {
-  let temp = 0;
   if (arr.length < 2) return arr;
-
-  if (arr.length === 2) {
-    if (arr[0] > arr[1]) {
-      temp = arr[0];
-      arr[0] = arr[1];
-      arr[1] = temp;
-    }
-  }
 
   const more = [];
   const less = [];
-  const first = arr[0];
+  const support = arr[0];
 
-  for ( let i = 1; i < arr.length; i++) {
-    if (arr[i] > first) {
-      more.push(arr[i]);
-    } else {
-      less.push(arr[i]);
-    }
+  for (let i = 1; i < arr.length; i++) {
+    (arr[i] > support) ? more.push(arr[i]) : less.push(arr[i]);
   }
 
-  return quickSort(less).concat(arr[0]).concat(quickSort(more));
+  return quickSort(less).concat(support).concat(quickSort(more));
 }
 
 const arrQuickSort = [10, 5, 2, 3, 33, 2, 8, 10];
